390c5053a251ca4384d1cde6c8fbaf2385139ed0
Tanmay Kadam <tanmaykadam260802@gmail.com>
2026-03-14 19:09:12 -0400

    added task files from ui and also then rendered those task cards

--- mini-project2-nodejs/index.js
+++ mini-project2-nodejs/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -12,8 +13,28 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res, next) => {
-    res.render("index");
+app.get("/", (req, res) => {
+    fs.readdir(`./files`, (err, files) => {
+        console.log(files);
+        files.forEach((files) => {
+            fs.readFile(`./files/${files}`, (err, data) =>{
+                console.log(data.toString());
+            });
+        })
+        
+        res.render("index", {files: files});
+    })
+});
+
+app.post("/create", (req, res) => {
+    // console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description, (err)=>{
+        if(err) {
+            console.log(err);
+            return res.status(500).send("Error Writing the file");
+        }
+        res.redirect('/')
+    });
 });
 
 app.listen(3000, () => {
